c55183491494da5fb66fe6f14500157614061a4c
Sachin Dharmapurikar <sachin@dharmapurikar.in>
2017-07-15 17:24:10 +0530

    Low power mode enabled.

--- talking_tom/talking_tom.ino
+++ talking_tom/talking_tom.ino
@@ -1,3 +1,5 @@
+#include "LowPower.h"
+
 int BTN_PIN = 3;
 int REC_LED = 4;
 int PLAY_PIN = 5;
@@ -17,34 +19,29 @@ void setup() {
   delay(100);
   digitalWrite(PLAY_PIN, LOW);
   delay(100);
-//  Serial.begin(9600);
 }
 
 void loop() {
   if (state == HIGH) {
-//    Serial.println("Record...");
     digitalWrite(REC_PIN, HIGH);
     delay(4000);
     digitalWrite(REC_PIN, LOW);
     delay(100);
-//    Serial.println("Play...");
     digitalWrite(PLAY_PIN, HIGH);
     delay(4000);
     digitalWrite(PLAY_PIN, LOW);
     delay(100);
-//    Serial.println("Stop...");
     count = count + 1;
     if (count >= 5) {
       count = 0;
-//      Serial.println("Reset");
       state = !state;
+      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
     }
   }
 }
 
 void start() {
   if (state == LOW) {
-//    Serial.println("Activating circuit!");
     state = !state;
   }
 }
